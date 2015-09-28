54d31fc075863037598977de7146a43671e7cc17
Salman <shash24@hotmail.ca>
2015-09-28 10:38:53 -0400

    Prepare server socket

--- WebServer.py
+++ WebServer.py
@@ -4,3 +4,18 @@ from socket import *
 
 # Create a server TCP socket
 serverSocket = socket(AF_INET, SOCK_STREAM)
+
+# Prepare server socket
+
+serverHost = 'localHost'
+recvBuffer = '1024'			# receive buffer
+serverPort = 12000			# Port number
+
+# Bind the serverSocket to port 12000
+serverSocket.bind(('',serverPort))
+										
+# serverSocket will Listen to one connection at a time
+# i.e. the max number of queued connections is 1
+serverSocket.listen(1)
+
+print 'The server is ready to receive'
